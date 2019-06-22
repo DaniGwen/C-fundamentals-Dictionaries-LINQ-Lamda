16ccc0c04378a258eceb1bb56e170541e8be77b2
Daniel <32326750+DaniGwen@users.noreply.github.com>
2019-06-22 16:36:16 +0300

    Add files via upload

---
+++ 1. Odd Occurrences/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1._Odd_Occurrences
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var dict = new Dictionary<string, int>();
+            var list = new List<string>();
+
+            var input = Console.ReadLine().ToLower().Split();
+
+            foreach (var word in input)
+            {
+                if (!dict.ContainsKey(word))
+                    dict.Add(word, 1);
+
+                else
+                    dict[word]++;
+            }
+
+            foreach (var word in dict.Where(w => w.Value % 2 == 1))
+            {
+                list.Add(word.Key);
+            }
+
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
