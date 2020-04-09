1df11a54e5c6f3f6aee8a0819f7bff99cd4258be
Daniel N <DaniGwen@users.noreply.github.com>
2020-04-09 21:10:14 +0300

    Solved ActivationKeys
    Added new Solution EmojiDetector

--- ProgrammingFundamentalsFinalExam04.04.2020/Problem3.HeroesofCodeandLogicVII/StartUp.cs
+++ ProgrammingFundamentalsFinalExam04.04.2020/Problem3.HeroesofCodeandLogicVII/StartUp.cs
@@ -1,31 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Problem3.HeroesofCodeandLogicVII
 {
     class Hero
     {
-        private const int MaxHitPoint = 100;
-        private const int MaxManaPoints = 100;
+        private const int MaxHitPoints = 100;
+        private const int MaxManaPoints = 200;
 
         private int manaPoints;
         private int hitPoints;
 
-        public Hero(string name, int hitPoints, int manaPoints)
+        public Hero()
+        {
+        }
+
+        public Hero(string name, int hitPoints, int manaPoints) : this()
         {
             this.Name = name;
             this.HitPoints = hitPoints;
             this.ManaPoints = manaPoints;
         }
 
-        public string Name { get; set; }
+        public string Name { get; private set; }
 
         public int HitPoints
         {
             get { return hitPoints; }
-            set
+            private set
             {
-                if (value > MaxHitPoint)
+                if (value > MaxHitPoints)
                 {
                     throw new ArgumentException("Hit points can't be more than 100");
                 }
@@ -36,7 +41,7 @@ namespace Problem3.HeroesofCodeandLogicVII
         public int ManaPoints
         {
             get { return manaPoints; }
-            set
+            private set
             {
                 if (value > MaxManaPoints)
                 {
@@ -44,6 +49,76 @@ namespace Problem3.HeroesofCodeandLogicVII
                 }
                 manaPoints = value;
             }
+        }
+
+        public bool IsDead { get; private set; }
+
+        internal void CastSpell(int mpNeeded, string spellName)
+        {
+            if (this.ManaPoints >= mpNeeded)
+            {
+                this.ManaPoints -= mpNeeded;
+                Console.WriteLine($"{this.Name} has successfully cast {spellName} and now has {this.ManaPoints} MP!");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Name} does not have enough MP to cast {spellName}!");
+            }
+        }
+
+        internal void TakeDamage(int damage, string attacker)
+        {
+            if (this.HitPoints > 0 && this.HitPoints > damage)
+            {
+                this.HitPoints -= damage;
+
+                Console.WriteLine($"{this.Name} was hit for {damage} HP by {attacker} and now has {this.HitPoints} HP left!");
+            }
+            else
+            {
+                this.IsDead = true;
+
+                Console.WriteLine($"{this.Name} has been killed by {attacker}!");
+            }
+        }
+
+        internal void Recharge(int rechargeAmount)
+        {
+            var currentManaPoints = this.ManaPoints;
+            var difference = 0;
+
+            if (rechargeAmount > MaxManaPoints || (currentManaPoints += rechargeAmount) > MaxManaPoints)
+            {
+                difference = MaxManaPoints - this.ManaPoints;
+                this.ManaPoints = MaxManaPoints;
+            }
+            else
+            {
+               this.ManaPoints += rechargeAmount;
+                difference = rechargeAmount;
+            }
+
+            Console.WriteLine($"{this.Name} recharged for {difference} MP!");
+        }
+
+        internal void Heal(int healAmount)
+        {
+            var difference = 0;
+            var currentHitPoints = this.HitPoints;
+
+            if ((healAmount + currentHitPoints) > MaxHitPoints)
+            {
+                difference = MaxHitPoints - this.HitPoints;
+                this.HitPoints = MaxHitPoints;
+            }
+            else
+            {
+                this.HitPoints += healAmount;
+
+                difference = healAmount;
+            }
+
+            Console.WriteLine($"{this.Name} healed for {difference} HP!");
         }
     }
 
@@ -55,7 +130,9 @@ namespace Problem3.HeroesofCodeandLogicVII
             var numberOfHeroes = int.Parse(Console.ReadLine());
 
             var listHeroes = new List<Hero>();
+            Hero hero = new Hero();
 
+            //Add heroes to list
             for (int i = 0; i < numberOfHeroes; i++)
             {
                 var heroArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
@@ -65,7 +142,7 @@ namespace Problem3.HeroesofCodeandLogicVII
                 var heroHitPoints = int.Parse(heroArgs[1]);
                 var heroManaPoints = int.Parse(heroArgs[2]);
 
-                var hero = new Hero(heroName, heroHitPoints, heroManaPoints);
+                hero = new Hero(heroName, heroHitPoints, heroManaPoints);
 
                 listHeroes.Add(hero);
             }
@@ -76,8 +153,60 @@ namespace Problem3.HeroesofCodeandLogicVII
 
                 if (input == "End")
                 {
-
+                    break;
                 }
+
+                var commandArgs = input.Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+
+                var command = commandArgs[0];
+                var heroName = commandArgs[1];
+
+                switch (command)
+                {
+                    case "CastSpell":
+                        var manaNeeded = int.Parse(commandArgs[2]);
+                        var spellName = commandArgs[3];
+
+                        hero = listHeroes.First(h => h.Name == heroName);
+
+                        hero.CastSpell(manaNeeded, spellName);
+                        break;
+                    case "TakeDamage":
+                        var damage = int.Parse(commandArgs[2]);
+                        var attacker = commandArgs[3];
+
+                        hero = listHeroes.First(h => h.Name == heroName);
+                        hero.TakeDamage(damage, attacker);
+
+                        if (hero.IsDead)
+                        {
+                            listHeroes.Remove(hero);
+                        }
+
+                        break;
+                    case "Recharge":
+                        var rechargeAmount = int.Parse(commandArgs[2]);
+
+                        hero = listHeroes.First(h => h.Name == heroName);
+                        hero.Recharge(rechargeAmount);
+                        break;
+                    case "Heal":
+                        var healAmount = int.Parse(commandArgs[2]);
+
+                        hero = listHeroes.First(h => h.Name == heroName);
+                        hero.Heal(healAmount);
+                        break;
+                }
+            }
+            var sortedHeroes = listHeroes.OrderByDescending(h => h.HitPoints)
+                    .ThenBy(h => h.Name)
+                    .ToList();
+
+            foreach (var sortedHero in sortedHeroes)
+            {
+                Console.WriteLine(sortedHero.Name);
+                Console.WriteLine($"  HP: {sortedHero.HitPoints}");
+                Console.WriteLine($"  MP: {sortedHero.ManaPoints}");
             }
         }
     }
